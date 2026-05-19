64f0ced42340d3d3275c1989285e0834abffac52
Third <rafi.34hack@gmail.com>
2026-05-19 20:45:29 +0700

    fix: clock validation for reservation form not calculating the right output

--- src/controller/reservasi_controller.py
+++ src/controller/reservasi_controller.py
@@ -63,6 +63,9 @@ class ReservasiController:
         if self._data_repository.cek_tanggal_dalam_maintenance(id_fasilitas, tanggal):
             return False
 
+        if jam_selesai <= jam_mulai:
+            return False
+
         if not self.validasi_jadwal(id_fasilitas, tanggal, jam_mulai, jam_selesai):
             return False
 
@@ -81,7 +84,9 @@ class ReservasiController:
             status=StatusReservasi.BELUM_DIBAYAR
         )
 
-        self._data_repository.tambah_reservasi(reservasi_baru)
+        berhasil = self._data_repository.tambah_reservasi(reservasi_baru)
+        if not berhasil:
+            return False
         self._notifikasi_controller.simpan_menit_sebelum(jam_notifikasi_sebelum)
         return True
         
@@ -139,8 +144,8 @@ class ReservasiController:
         detik_mulai = (jam_mulai.hour * 3600) + (jam_mulai.minute * 60) + jam_mulai.second
         detik_selesai = (jam_selesai.hour * 3600) + (jam_selesai.minute * 60) + jam_selesai.second
         durasi_detik = detik_selesai - detik_mulai
-        if durasi_detik < 0:
-            durasi_detik += 86400
+        if durasi_detik <= 0:
+            return Decimal("0")
         durasi_jam = Decimal(str(durasi_detik)) / Decimal('3600')
         return durasi_jam * harga_per_jam
         
